545be6774584ddd88fd5eb9cd8eb561d501723ce
Rakeeb Rajbhandari <rakeeb@Rakeebs-MacBook-Pro.local>
2018-02-15 23:33:48 +0545

    * switch mapping * add roboelectric * add rxbinding

--- app/src/test/java/com/infinite/rx/OperatorsTest.kt
+++ app/src/test/java/com/infinite/rx/OperatorsTest.kt
@@ -1,12 +1,13 @@
 package com.infinite.rx
 
+import android.widget.EditText
 import com.infinite.rx.challenge.week1.simpleMapping
-import com.infinite.rx.challenge.week2.User
-import com.infinite.rx.challenge.week2.userConcatMapStream
-import com.infinite.rx.challenge.week2.userFlatMapStream
+import com.infinite.rx.challenge.week2.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.BlockJUnit4ClassRunner
+import org.robolectric.RuntimeEnvironment
+import java.util.concurrent.TimeUnit
 
 @RunWith(BlockJUnit4ClassRunner::class)
 class OperatorsTest {
@@ -37,5 +38,34 @@ class OperatorsTest {
         flattenedStream
                 .assertComplete()
                 .assertValueCount(10)
+    }
+
+    @Test
+    fun switchMapping() {
+        val switcher = switchMapStream.test()
+        switcher.awaitTerminalEvent()
+        switcher
+                .assertValue("fx")
+                .assertValueCount(1)
+                .assertComplete()
+    }
+
+    @Test
+    fun searchText() {
+        val context = RuntimeEnvironment.application
+        val userQuery = EditText(context)
+        userQuery.setText("a")
+        userQuery.setText("b")
+        userQuery.setText("c")
+        userQuery.setText("d")
+        userQuery.setText("e")
+        userQuery.setText("f")
+        val stream = searchResultStream(userQuery)
+                .delay(100, TimeUnit.MILLISECONDS)
+                .test()
+
+        stream.awaitTerminalEvent()
+        stream
+                .assertComplete()
     }
 }
